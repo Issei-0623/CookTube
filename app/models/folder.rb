8462df7e4afdee87bc5b6ec2aaecb65d96986a94
issei <proteinebimayoduu@gmail.com>
2025-11-03 18:07:52 +0900

    フォルダ編集画面、フォルダ削除機能、バリデーション（日本語対応）の実装

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,3 +1,7 @@
 class Folder < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :user_id, message: "はすでに存在します" } # 同じユーザー内で同名フォルダを禁止（他ユーザーならOK）
+
 end
