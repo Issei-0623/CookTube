c16eb395a42201810514e355ae1a4b39778ddd48
issei <proteinebimayoduu@gmail.com>
2025-11-09 18:07:20 +0900

    保存動画のフォルダ移動機能を実装

--- app/controllers/saved_videos_controller.rb
+++ app/controllers/saved_videos_controller.rb
@@ -1,6 +1,16 @@
 class SavedVideosController < ApplicationController
   def index
-    @saved_videos = current_user.saved_videos
+    @saved_videos = current_user.saved_videos.where(folder_id: nil)
+    @folders = current_user.folders
+  end
+
+  def update
+    @video = current_user.saved_videos.find(params[:id])
+    if @video.update(folder_id: params[:folder_id])
+      redirect_to saved_videos_path, notice: "動画を移動しました"
+    else
+      redirect_to saved_videos_path, alert: "移動に失敗しました"
+    end
   end
 
   def create
